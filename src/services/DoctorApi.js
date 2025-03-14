9b3a7db6288acac124497f645aef1d8afc9da6e0
Thanh Thảo <thaontt08032003@gmail.com>
2025-03-14 13:09:58 +0700

    change logic of Tu

--- src/services/DoctorApi.js
+++ src/services/DoctorApi.js
@@ -2,39 +2,28 @@ import api from "../config/axios";
 
 const doctorApi = {
   getAllDoctors: async () => {
-    try {
-      const response = await api.get("/Doctors");
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
+    const response = await api.get("/Doctors");
+    return response.data;
   },
 
   getDoctorSpecializations: async (doctorId) => {
-    try {
-      const response = await api.get(`/Specializations/${doctorId}`);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
+    const response = await api.get(`/Specializations/${doctorId}`);
+    return response.data;
   },
 
   getConsultationRequests: async () => {
-    try {
-      const response = await api.get("/ConsultationRequests");
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
+    const response = await api.get("/ConsultationRequests");
+    return response.data;
   },
 
   createConsultationRequest: async (data) => {
-    try {
-      const response = await api.post("/ConsultationRequests", data);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
+    const response = await api.post("/ConsultationRequests", data);
+    return response.data;
+  },
+
+  getConsultationResponse: async () => {
+    const response = await api.get("/ConsultationResponses");
+    return response.data;
   },
 };
 
